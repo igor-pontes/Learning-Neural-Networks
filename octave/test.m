c52471ef5531d48011d481244ce2cadd1634095e
Igor Pontes <igoppop@gmail.com>
2021-04-20 16:36:27 +0100

    Week 1

--- octave/test.m
+++ octave/test.m
@@ -26,3 +26,44 @@ lenght(V) % gives the size of the longest dimension (in this case 4)
 lenght(A) % = 3
 
 lenght([1;2;3;4;5]) % = 5
+
+% load('data1.dat') - loads file with your data and creates, in this case, a var named
+% "data1", with your data.
+
+who % variables in the current scope.
+whos % show variables in the current scope with detailed information
+
+V(1:3) % displays the first 3 items of the vector "V"
+
+% save test.mat V; - will save variable "V" in a file called test.mat in binary format.
+
+clear - % deletes all the variables in the workspace;
+
+% save hello.txt v -ascii - saves data in a more human readable format (ASCII).
+A = [1 2; 3 4; 5 6]
+
+A(3, 2) % gives us the A3,2 element. (= 6)
+
+% A(2,:) - ":" means every element along that row/column
+
+% A(:,2) - gives us everything in the second column
+
+A([1 3], :) % gives us every element in the first and third row
+
+A(:,2) = [10;11;12] % taking the second column of "A" and assigning this vector to it
+
+A = [A, [100; 101; 102]]; % append another column vector to the right of "A"
+
+% [100; 101; 102] - column vector
+
+A(:) % put all elements of "A" into a single column vector
+
+A = [1 2; 3 4; 5 6];
+
+B = [11 12; 13 14; 15 16];
+
+C = [A B] % concatenate matrix "A" and "B" to eachother
+
+C = [A; B] % instead of concatenating them horizontally, concatenate them vertically 
+
+% [A B] == [A, B] - same result
