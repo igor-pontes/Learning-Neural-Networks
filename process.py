ec987b37fbe28cebcd277a0af5b4b9a6a2fc8c8d
Igor Pontes <igoppop@gmail.com>
2021-07-13 00:21:47 +0100

    okok

---
+++ process.py
@@ -0,0 +1,40 @@
+import os
+
+def processfile(imagefile):
+    images = []
+    labels = []
+    content = []
+    file = open(os.path.dirname(os.path.realpath(__file__))+"/dataset/"+imagefile, 'rb')
+    try: content = file.read()
+    finally: file.close()
+    magic_number = int.from_bytes(content[0:4], "big")
+    total = int.from_bytes(content[4:8], "big")
+    c = 0
+    if magic_number == 2051:
+        rows = int.from_bytes(content[8:12], "big")
+        columns = int.from_bytes(content[12:16], "big")
+        i = 16
+        while c < total:
+            try:
+                image = []
+                image = content[i:i+(rows*columns)]
+                images.append(image)
+                i = i+(rows*columns)
+            except IndexError:
+                print("Error")
+                break
+            c += 1
+        return images
+    if magic_number == 2049:
+        i = 8
+        while c < total:
+            try:
+                labels.append(content[i])
+                i += 1
+            except IndexError:
+                print("Error")
+                break
+            c += 1
+        return labels
+
+data = processfile("train-images-idx3-ubyte")
