03634827cfae9ccd2c15b69350975a979332bdd3
Igor Pontes <igoppop@gmail.com>
2021-07-22 21:01:29 +0100

    almost there

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,22 +3,39 @@ import math
 class NeuralNetwork(object):
     def __init__(self, *args): 
         self.layers = [np.ones((1,args[x]+1)) if x < len(args)-1 else np.ones((1,args[x])) for x in range(len(args))]
-        self.weights = [np.random.randn(self.layers[x+1].shape[1],self.layers[x].shape[1])*np.sqrt(2/self.layers[x].shape[1]) for x in range(len(args)-1)] # He-et-al Initialization
+        # He-et-al Initialization
+        self.weights = [np.random.randn(self.layers[x+1].shape[1],self.layers[x].shape[1])*np.sqrt(2/self.layers[x].shape[1]) for x in range(len(args)-1)]
+    
+    def sigmoidgradient(self, x):
+        # X with sigmoid func
+        return x*(1-x) 
     
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
     
-    def fowardprop(self, image = None):
+    def forwardprop(self, image = None):
         temp = [1]
         temp.extend(image)
-        self.layers[0] = np.transpose(np.asarray(temp, dtype=np.float32).reshape(len(temp),1))
+        self.layers[0] = np.transpose(np.asarray(temp).reshape(len(temp),1))
         for i, w in zip(range(1,len(self.layers)), self.weights):
             self.layers[i] = self.sigmoid(np.matmul(self.layers[i-1], np.transpose(w)))
-            print(self.layers[i])
-       # return np.where(self.layers[-1] == np.amax(self.layers[-1]))[1]
+        return np.where(self.layers[-1] == np.amax(self.layers[-1]))[1]
 
     def backprop(self, m):
-        None 
-    
+        delta = [0, 0]
+        for i in m[0:20]:
+            error = []
+            self.forwardprop(i[0])
+            error.extend(self.layers[-1]-i[1])
+
+            for l, w, e in zip(range(len(self.layers)-2, 0, -1), range(len(self.weights)-1, 0, -1), range(0, len(self.layers)-2)):
+                error[e] = error[e].reshape(len(error[e]), 1)
+                error.extend([np.matmul(np.transpose(self.weights[w][:,:]),error[e])*self.sigmoidgradient(np.transpose(self.layers[l][:,:]))])
+            for d, l in zip(range(0, len(error)), range(len(self.layers)-2, -1,-1)):
+                delta[d] += np.matmul(error[d],self.layers[l])
+        
+        for w, d in zip(range(0, len(delta)), range(len(delta)-1, -1, -1)):
+            self.weights[w] = delta[d]
+        
     def sgd(self): # TODO
         None
--- testing.py
+++ testing.py
@@ -3,11 +3,15 @@ from PIL import Image
 import numpy as np
 from process import processfile
 from NeuralNetwork import NeuralNetwork
-data = processfile("train-images-idx3-ubyte")
-images = [np.asarray(Image.frombytes(mode = "L", size=(len(data[0]),1), data=data[x]), dtype=np.float32).flatten() for x in range(len(data))]
-#i_array = np.asarray(image).flatten()
-n = NeuralNetwork(len(images[0]),20,10)
-lol = n.fowardprop(images[0])
+data_i = processfile("train-images-idx3-ubyte")
+data_l = processfile("train-labels-idx1-ubyte")
+images = [(np.asarray(Image.frombytes(mode = "L", size=(len(data_i[0]),1), data=data_i[x]), dtype=np.float32).flatten(), data_l[x]) for x in range(len(data_i))]
+n = NeuralNetwork(len(images[0][0]),20,10)
+#lol = n.fowardprop(images[0][0])
 #print(lol)
+n.backprop(images)
+print(n.forwardprop(images[9][0]))
+print("---")
+print(images[9][1])
 #image.save("number.jpeg")
 
