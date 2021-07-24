da26055d8641b2345d0d1d7043a9a31900d8ee03
Igor Pontes <igoppop@gmail.com>
2021-07-24 15:40:26 +0100

    :)

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,11 +4,13 @@ class NeuralNetwork(object):
     def __init__(self, *args): 
         self.layers = [x for x in args]
         self.weights = [np.random.randn(self.layers[x+1],self.layers[x]+1)*np.sqrt(2/self.layers[x]) for x in range(len(args)-1)]
+
     def sigmoidgradient(self, x):
         # X with sigmoid func
         return x*(1-x) 
     
     def sigmoid(self, x):
+        x = np.clip( x, -500, 500 )
         return 1./(1.+np.exp(-x))
     
     def forwardprop(self, image = None):
@@ -25,22 +27,17 @@ class NeuralNetwork(object):
 
     def backprop(self, m, lamb):
         delta = np.zeros(len(self.weights), dtype = object)
-        m = m[0:10000]
+        m = m[0:60000]
         for i in m:
             error = np.empty(len(self.weights), dtype = object)
             a = self.forwardprop(i[0])
             error[0] = np.asarray((a[-1]-i[1]).transpose(), dtype=np.float128)
            
             for e, w, l in zip(range(1, len(self.weights)), range(len(self.weights)-1, -1, -1), range(len(a)-2, -1, -1)):
-                #error[e] = error[e].reshape(error[e].shape[1], 1)
                 error[e] = np.asarray(np.matmul(self.weights[w].transpose()[1:,:], error[e-1]) * self.sigmoidgradient(a[l].transpose()[1:,:]), dtype=np.float128)
                   
             for d, l in zip(range(0, len(error)), range(len(self.layers)-2, -1,-1)):
-                #if d == 0:
-                #delta[d] = np.zeros((error[d].shape[0], a[l].shape[1]), dtype = np.float128)
                 delta[d] += np.matmul(error[d],a[l])
-                #else:
-                #    delta[d] += np.matmul(error[d],a[l])[1:]
         for w, d in zip(range(0, len(delta)), range(len(delta)-1, -1, -1)):
             self.weights[w] = np.concatenate(( (1/len(m)) * delta[d][:,0].reshape(delta[d][:,0].shape[0],1), (lamb/len(m)) * delta[d][:,1:]), axis=1)
 
