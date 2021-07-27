5adb6897593a18cf2db7f1ffe3a2f87f1174a799
Igor Pontes <igoppop@gmail.com>
2021-07-27 20:19:11 +0100

    nice

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,54 +3,67 @@ import math
 class NeuralNetwork(object):
     def __init__(self, *args): 
         self.layers = [x for x in args]
-        self.weights = [np.random.randn(self.layers[x+1],self.layers[x]+1)*np.sqrt(2/self.layers[x]) for x in range(len(args)-1)]
-
-    def sigmoidgradient(self, x):
-        # X with sigmoid func
-        return x*(1-x) 
+        self.weights = [np.random.randn(x, y + 1) for x, y in zip(self.layers[1:], self.layers[:-1])]
+        self.sizes = [x + 1 if x != args[-1] else x for x in args]
+   
+    def sigmoidprime(self, x):
+        return self.sigmoid(x)*(1-self.sigmoid(x)) 
     
     def sigmoid(self, x):
-        x = np.clip(x, -600, 600)
+        x = np.clip(x, -900, 900)
         return 1./(1.+np.exp(-x))
     
     def forwardprop(self, image = None):
-        a = np.empty(len(self.layers), dtype = object)
-        temp = [1]
-        temp.extend(image)
-        a[0] = np.asarray(np.transpose(np.asarray(temp).reshape(len(temp),1)), dtype=np.float128)
-        for i, w in zip(range(1,len(self.layers)), self.weights):
-            if i == len(self.layers)-1:
-                a[i] = self.sigmoid(np.asarray(np.matmul(a[i-1], np.transpose(w)), dtype=np.float128))
-            else:
-                a[i] = np.concatenate((np.ones((1,1)), self.sigmoid(np.asarray(np.matmul(a[i-1], np.transpose(w)), dtype=np.float128))), axis=1)
-        return a
+        a = [np.ones((x,1), dtype = np.float128) for x in self.sizes]
+        z = [np.ones((x,1), dtype = np.float128) for x in self.sizes]
 
-    def backprop(self, m, lamb):
-        delta = np.zeros(len(self.weights), dtype = object)
-        lr = 1
-        m = m[0:10000]
+        a[0][1:] = np.asarray(image).reshape(len(image), 1)
+        z[0] = a[0]
+        
+        for w, l in zip(self.weights[:-1], range(1,len(a)-1)):
+            a[l] = np.vstack((np.ones((1,1)), self.sigmoid(np.dot(w, a[l-1]))))
+            z[l] = np.vstack((np.ones((1,1)), np.dot(w, a[l-1])))
+        
+        a[-1] = self.sigmoid(np.dot(self.weights[-1], a[-2]))
+        z[-1] = np.dot(self.weights[-1], a[-2])
+
+        return (z, a)
+
+    def gd(self, data, epochs, lr, lamb):
+        for x in range(0, epochs):
+            self.backprop(data, lr, lamb)
+            print("Epoch {0} accuracy: {1}%".format(x, self.accuracy(data[0:1000])))
+
+    def backprop(self, m, lr, lamb):
+        m = m[0:60000]
+        size = len(m)
+        delta = [np.zeros(w.shape, dtype = np.float128) for w in self.weights]
         for i in m:
-            error = np.empty(len(self.weights), dtype = object)
-            a = self.forwardprop(i[0])
-            output = np.array([0 if x != i[1] else 1 for x in range(0,a[-1].shape[1])])
-            error[0] = np.asarray((a[-1]-output).transpose(), dtype=np.float128)
-           
-            for e, w, l in zip(range(1, len(self.weights)), range(len(self.weights)-1, -1, -1), range(len(a)-2, -1, -1)):
-                error[e] = np.asarray(np.matmul(self.weights[w].transpose()[1:,:], error[e-1]) * self.sigmoidgradient(a[l].transpose()[1:,:]), dtype=np.float128)
-                  
-            for d, l in zip(range(0, len(error)), range(len(self.layers)-2, -1,-1)):
-                delta[d] += np.matmul(error[d],a[l])
-            
-            for w, d in zip(range(0, len(delta)), range(len(delta)-1, -1, -1)):
-                self.weights[w] = self.weights[w] - lr*np.concatenate(( (1/len(m)) * delta[d][:,0].reshape(delta[d][:,0].shape[0],1), (lamb/len(m)) * delta[d][:,1:]), axis=1)
+            error = [np.zeros(self.sizes[x], dtype = np.float128) for x in range(1, len(self.sizes))]    
+            output = np.asarray([0 if x != i[1] else 1 for x in range(0,self.layers[-1])]).reshape(self.layers[-1], 1)
+            z, a = self.forwardprop(i[0])
+            error[-1] = a[-1] - output
 
+            for w, e in zip(range(1, len(self.weights)), range(1, len(self.layers))):
+                error[-e-1] = np.dot(self.weights[-w].transpose(), error[-e]) * self.sigmoidprime(z[-e-1])
+            for e, l in zip(range(0, len(error)-1), range(0, len(self.layers)-1)):
+                delta[e] += np.dot(error[e][1:,:], a[l].transpose())
 
-        #for w, d in zip(range(0, len(delta)), range(len(delta)-1, -1, -1)):
-        #    self.weights[w] = np.concatenate(( (1/len(m)) * delta[d][:,0].reshape(delta[d][:,0].shape[0],1), (lamb/len(m)) * delta[d][:,1:]), axis=1)
+            delta[-1] += np.dot(error[-1], a[-2].transpose())
+        
+        for w in range(0, len(self.weights)):
+            temp = np.hstack(((1/size)*delta[w][:,0].reshape(delta[w][:,0].shape[0], 1), (1/size)*delta[w][:,1:] + (lamb/size)*self.weights[w][:,1:]))
+            self.weights[w] = self.weights[w] - lr*temp
 
     def predict(self, image):
-        l = self.forwardprop(image)[-1]
-        return np.argmax(l)
+        _, l = self.forwardprop(image)
+        return np.argmax(l[-1])
 
-    def sgd(self): # TODO
-        None
+    def accuracy(self, data):
+        r = 0
+        for x in data:
+           i = self.predict(x[0])
+           if i == x[1]:
+               r += 1
+        return r/len(data)*100
+    
--- testing.py
+++ testing.py
@@ -7,9 +7,9 @@ data_i = processfile("train-images-idx3-ubyte")
 data_l = processfile("train-labels-idx1-ubyte")
 images = [(np.asarray(Image.frombytes(mode = "L", size=(len(data_i[0]),1), data=data_i[x]), dtype=np.float32).flatten(), data_l[x]) for x in range(len(data_i))]
 
-n = NeuralNetwork(len(images[0][0]), 15, 10)
-n.backprop(images, 3)
-image = images[1]
+n = NeuralNetwork(len(images[0][0]), 30, 10)
+n.gd(images, 30, 3, 3)
+image = images[0]
 print(n.predict(image[0]))
 print("---")
 print(image[1])
